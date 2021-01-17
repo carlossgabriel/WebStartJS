27848bab54a6b391a57ebfdfc10a7bfcefdeb7f0
Carlos Gabriel <cgsalbuquerque@gmail.com>
2021-01-16 23:57:19 -0300

    :sparkles: css center elmts+ 🔧 change html via js

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ function changeTitle() {
     var x = document.getElementById('title');
     if (x.style.display === 'none') {
         x.style.display = 'block';
+        document.getElementById("buttonTitle").innerHTML = "Click to hide the title"
     } else {
         x.style.display = 'none';
+        document.getElementById("buttonTitle").innerHTML = "Click to display the title"
     }
 }
