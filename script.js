b0ef64a9db5786bc85922eb501c034554bb0ac81
Carlos Gabriel <cgsalbuquerque@gmail.com>
2021-01-17 23:31:20 -0300

    ✨add sidebar effect and CSS

--- script.js
+++ script.js
@@ -13,3 +13,13 @@ function showTime() {
     var today = new Date();
     document.getElementById('time').innerHTML = today.getDate() + "/" + today.getMonth()+1 + "/" + today.getFullYear();
 }
+
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "250px";
+  }
+  
+  /* Set the width of the sidebar to 0 (hide it) */
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
