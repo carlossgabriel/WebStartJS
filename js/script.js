c20c03efdd2f5eb585e29678ca7eac02cd037119
Carlos Gabriel <cgsalbuquerque@gmail.com>
2021-01-20 22:46:23 -0300

    Updating the dashboard desing CSS color and path

--- js/script.js
+++ js/script.js
@@ -1,25 +1,14 @@
-function changeTitle() {
-    var x = document.getElementById('title');
-    if (x.style.display === 'none') {
-        x.style.display = 'block';
-        document.getElementById("buttonTitle").innerHTML = "Click to hide the title"
-    } else {
-        x.style.display = 'none';
-        document.getElementById("buttonTitle").innerHTML = "Click to display the title"
-    }
-}
-
 function showTime() {
     var today = new Date();
-    document.getElementById('time').innerHTML = today.getDate() + "/" + today.getMonth()+1 + "/" + today.getFullYear();
+    document.getElementById('time').innerHTML = today.getDate() + "/" + today.getMonth() + 1 + "/" + today.getFullYear();
 }
 
 /* Set the width of the sidebar to 250px (show it) */
 function openNav() {
     document.getElementById("mySidepanel").style.width = "250px";
-  }
-  
-  /* Set the width of the sidebar to 0 (hide it) */
-  function closeNav() {
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
     document.getElementById("mySidepanel").style.width = "0";
-  }
+}
